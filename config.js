95a41f1c7b374882fb46cae66cd018533b1b8859
David Reke <david.e.reke@gmail.com>
2020-08-31 11:32:05 -0400

    updat axios urls in react components

--- config.js
+++
@@ -1,4 +0,0 @@
-const dotenv =require('dotenv')
-dotenv.config()
-
-export const BACKEND_URL= process.env.NODE_ENV === 'development'?"http://localhost:5000": "https://reke-exercise-tracker.herokuapp.com" 
